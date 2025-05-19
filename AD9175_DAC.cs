2c08498970f1175f09ff6e3e29381bff6b4166ae
Alexey Tseirif IL <alexey.tseirif@satixfy.com>
2025-05-19 13:15:59 +0300

    updates for DAC

--- AD9175_DAC.cs
+++ AD9175_DAC.cs
@@ -24,8 +24,17 @@ namespace BringUp_Control
             _ft = ft;
 
 
-            // TABLE 50  startup sequence
+            // Iinit of the DAC
             PowerUp();
+            DAC_PLL_Config();
+
+            DelayLockLoop();
+            Calibration();
+
+            JESD204B_Setup();
+            MainDAC_Datapath_Setup();
+
+
         }
 
         //Table 50 : Power Up registee writing
@@ -42,26 +51,161 @@ namespace BringUp_Control
         // Table 51: DAC and PLL configuration sequence
         public void DAC_PLL_Config()
         {
-            WriteRegister(0x0095, 0x01); // Power on DAC and bias circuity
-            WriteRegister(0x0790, 0xFF); // Power up clock receiver
-            WriteRegister(0x0791, 0xFF); // Power up PLL
-            WriteRegister(0x0796, 0xE5); // Power up clock receiver PLL
-            WriteRegister(0x07A0, 0xBC); // Power up clock receiver PLL
-            WriteRegister(0x0794, 0x04); // Power up clock receiver PLL
-            WriteRegister(0x0797, 0x10); // Power up clock receiver PLL
-            WriteRegister(0x0797, 0x20); // Power up clock receiver PLL
-            WriteRegister(0x0798, 0x10); // Power up clock receiver PLL
-            WriteRegister(0x07A2, 0x7F); // Power up clock receiver PLL
+            WriteRegister(0x0095, 0x01); // Bypass internal PLL
+            WriteRegister(0x0790, 0xFF); // Bypass internal PLL
+            WriteRegister(0x0791, 0xFF); // Bypass internal PLL
+            WriteRegister(0x0796, 0xE5); // DAC PLL required write value
+            WriteRegister(0x07A0, 0xBC); // DAC PLL required write value
+            WriteRegister(0x0794, 0x04); // DAC PLL charge pump current settings
+            WriteRegister(0x0797, 0x10); // DAC PLL required write value
+            WriteRegister(0x0797, 0x20); // DAC PLL required write value
+            WriteRegister(0x0798, 0x10); // DAC PLL required write value
+            WriteRegister(0x07A2, 0x7F); // DAC PLL required write value
             Thread.Sleep(100);  //delay 100ms
             WriteRegister(0x0799, 0xC8); // DAC PLL divider settings
-            WriteRegister(0x0793, 0x18); // DAC PLL divider settings
-            WriteRegister(0x0094, 0x00); // PLL VCO freq no
+            WriteRegister(0x0793, 0x18); // DAC PLL predivider settings
+            WriteRegister(0x0094, 0x00); // PLL VCO freq default
             WriteRegister(0x0792, 0x02); // Reset VCO
             WriteRegister(0x0792, 0x00); // 
             Thread.Sleep(100);  //delay 100ms
-            //WriteRegister(0x07B5, 0x00); // No need , not using PLL
+            // (0x07B5, 0x00); // No need , not using PLL
 
         }
+        // Table 52: Delay lock loop configuration sequence
+        public void DelayLockLoop()
+        {
+            WriteRegister(0x00C0, 0x00); // Power up delay line
+            WriteRegister(0x00DB, 0x00); // 
+            WriteRegister(0x00DB, 0x01); // Update DLL settings to circuityBypass internal PLL
+            WriteRegister(0x00DB, 0x00); // 
+            WriteRegister(0x00C1, 0x68); // Set DLL search mode Fdac > 4.5GHz
+            WriteRegister(0x00C1, 0x69); // Set DLL search mode Fdac > 4.5GHz
+            WriteRegister(0x00C7, 0x01); // Enable DLL read status
+            // (0x00C3, 0x01); // Ensure DLL is locked by reading value 1 for Bit0 of this register
+
+        }
+        // Table 53: Calibration sequence
+        public void Calibration()
+        {
+            WriteRegister(0x0050, 0x2A); // Optimized calibration setting register
+            WriteRegister(0x0061, 0x68); // Required calibration control register
+            WriteRegister(0x0051, 0x82); // Optimized calibration setting register
+            WriteRegister(0x0051, 0x83); // Required calibration control register 
+            WriteRegister(0x0081, 0x03); // Required calibration control register
+        }
+        // Table 54: JESD204B configuration sequence
+        public void JESD204B_Setup()
+        {
+            WriteRegister(0x0100, 0x00); // Power up digital datapath clocks
+            WriteRegister(0x0110, 0x37); // dual link and mode 23
+            WriteRegister(0x0111, 0x61); // Main datapath configuration
+            WriteRegister(0x0084, 0x01); // SYSREF singla input
+            WriteRegister(0x0312, 0x000000); // ?????????????????????? NO DATA
+            
+            //run LINK = 0
+            WriteRegister(0x0300, 0x08); // Corresponds to the mode selection made in register 0x110
+            WriteRegister(0x0475, 0x09); // Soft reset JESD2024B quad byte deframer
+            WriteRegister(0x0453, 0x03); // Set scrambling option for SERDES data
+            WriteRegister(0x0458, 0x11); // L value to JESD_MODE
+            WriteRegister(0x0475, 0x01); // Bring the JESD204B quad byte deframer out of reset
+            //run LINK = 1
+            WriteRegister(0x0300, 0x0C); // Corresponds to the mode selection made in register 0x110
+            WriteRegister(0x0475, 0x09); // Soft reset JESD2024B quad byte deframer
+            WriteRegister(0x0453, 0x00); // Set scrambling option for SERDES data
+            WriteRegister(0x0458, 0x00); // L value to JESD_MODE
+            WriteRegister(0x0475, 0x01); // Bring the JESD204B quad byte deframer out of reset
+        }
+
+        // Table 55: Channel datapath configuration sequence - CAN BE SKIPPED
+        public void ChannelDatapath_Setup()
+        {
+            
+        }
+
+        // Table 56: Main datapath and Main NCO configuration sequence
+        public void MainDAC_Datapath_Setup()
+        {
+
+            //DAC 0
+            WriteRegister(0x0008, 0x40); // SELECT DAC0
+            WriteRegister(0x0112, 0x38); // Enable NCO for selected channels in paging Register 0x008
+            WriteRegister(0x0114, 0x2B); // Write DDSM_FTW[7:0]
+            WriteRegister(0x0115, 0xC2); // Write DDSM_FTW[15:8]
+            WriteRegister(0x0116, 0xBC); // Write DDSM_FTW[23:16]
+            WriteRegister(0x0117, 0x2B); // Write DDSM_FTW[31:24]
+            WriteRegister(0x0118, 0xC2); // Write DDSM_FTW[39:32]
+            WriteRegister(0x0119, 0xBC); // Write DDSM_FTW[47:40]
+            WriteRegister(0x011C, 0x00); // Write DDSM_NCO_PHASE_OFFSET[7:0]
+            WriteRegister(0x011D, 0x00); // Write DDSM_NCO_PHASE_OFFSET[15:8]
+            
+            WriteRegister(0x0124, 0x00); // Write DDSM_ACC_MODULUS[7:0]
+            WriteRegister(0x0125, 0x00); // Write DDSM_ACC_MODULUS[15:8]
+            WriteRegister(0x0126, 0x00); // Write DDSM_ACC_MODULUS[23:16]
+            WriteRegister(0x0127, 0x00); // Write DDSM_ACC_MODULUS[31:24]
+            WriteRegister(0x0128, 0x00); // Write DDSM_ACC_MODULUS[39:32]
+            WriteRegister(0x0129, 0x00); // Write DDSM_ACC_MODULUS[47:40]
+            
+            WriteRegister(0x012A, 0x00); // Write DDSM_ACC_DELTA[7:0]
+            WriteRegister(0x012B, 0x00); // Write DDSM_ACC_DELTA[15:8]
+            WriteRegister(0x012C, 0x00); // Write DDSM_ACC_DELTA[23:16]
+            WriteRegister(0x012D, 0x00); // Write DDSM_ACC_DELTA[31:24]
+            WriteRegister(0x012E, 0x00); // Write DDSM_ACC_DELTA[39:32]
+            WriteRegister(0x012F, 0x00); // Write DDSM_ACC_DELTA[47:40]
+            WriteRegister(0x0113, 0x01); // Update all NCO phase and FTW words
+
+            //DAC 1
+            WriteRegister(0x0008, 0x80); // SELECT DAC0
+            WriteRegister(0x0112, 0x38); // Enable NCO for selected channels in paging Register 0x008
+            WriteRegister(0x0114, 0x41); // Write DDSM_FTW[7:0]
+            WriteRegister(0x0115, 0xA4); // Write DDSM_FTW[15:8]
+            WriteRegister(0x0116, 0x1A); // Write DDSM_FTW[23:16]
+            WriteRegister(0x0117, 0x41); // Write DDSM_FTW[31:24]
+            WriteRegister(0x0118, 0xA4); // Write DDSM_FTW[39:32]
+            WriteRegister(0x0119, 0x1A); // Write DDSM_FTW[47:40]
+            WriteRegister(0x011C, 0x00); // Write DDSM_NCO_PHASE_OFFSET[7:0]
+            WriteRegister(0x011D, 0x00); // Write DDSM_NCO_PHASE_OFFSET[15:8]
+
+            WriteRegister(0x0124, 0x00); // Write DDSM_ACC_MODULUS[7:0]
+            WriteRegister(0x0125, 0x00); // Write DDSM_ACC_MODULUS[15:8]
+            WriteRegister(0x0126, 0x00); // Write DDSM_ACC_MODULUS[23:16]
+            WriteRegister(0x0127, 0x00); // Write DDSM_ACC_MODULUS[31:24]
+            WriteRegister(0x0128, 0x00); // Write DDSM_ACC_MODULUS[39:32]
+            WriteRegister(0x0129, 0x00); // Write DDSM_ACC_MODULUS[47:40]
+
+            WriteRegister(0x012A, 0x00); // Write DDSM_ACC_DELTA[7:0]
+            WriteRegister(0x012B, 0x00); // Write DDSM_ACC_DELTA[15:8]
+            WriteRegister(0x012C, 0x00); // Write DDSM_ACC_DELTA[23:16]
+            WriteRegister(0x012D, 0x00); // Write DDSM_ACC_DELTA[31:24]
+            WriteRegister(0x012E, 0x00); // Write DDSM_ACC_DELTA[39:32]
+            WriteRegister(0x012F, 0x00); // Write DDSM_ACC_DELTA[47:40]
+            WriteRegister(0x0113, 0x01); // Update all NCO phase and FTW words
+        }
+
+
+        public void JESD204B_SERDES_Setup()
+        {
+            WriteRegister(0x0240, 0xAA); // EQ settings IN_Loss < 11dB value 0xAA
+            WriteRegister(0x0241, 0xAA); // EQ settings IN_Loss < 11dB value 0xAA
+            WriteRegister(0x0242, 0x55); // EQ settings IN_Loss < 11dB value 0x55
+            WriteRegister(0x0243, 0x55); // EQ settings IN_Loss < 11dB value 0x55
+            WriteRegister(0x0244, 0x1F); // EQ settings
+            WriteRegister(0x0245, 0x1F); // EQ settings
+            WriteRegister(0x0246, 0x1F); // EQ settings
+            WriteRegister(0x0247, 0x1F); // EQ settings
+            WriteRegister(0x0248, 0x1F); // EQ settings
+            WriteRegister(0x0249, 0x1F); // EQ settings
+            WriteRegister(0x024A, 0x1F); // EQ settings
+            WriteRegister(0x024B, 0x1F); // EQ settings
+            WriteRegister(0x0201, 0x00); // EQ settings
+            WriteRegister(0x0203, 0x00); // EQ settings
+            WriteRegister(0x0253, 0x01); // EQ settings
+            WriteRegister(0x0254, 0x01); // EQ settings
+            WriteRegister(0x0210, 0x16); // EQ settings
+            WriteRegister(0x0216, 0x11); // EQ settings
+
+
+        }
+
         public List<string> LoadComboRegister9175()
         {
             for (int i = 0x0000; i <= 0x07B5; i++)
@@ -218,10 +362,9 @@ namespace BringUp_Control
             return regaddresslist9175;
         }
 
-        public void BootSec()
-        {
 
-        }
+
+        
 
         public DataTable InitDataTableDAC()
         {
@@ -234,10 +377,7 @@ namespace BringUp_Control
             return dtAD9175;
         }
 
-        public void CleanUp_Register()
-        {
-
-        }
+        
 
         public void WriteRegister(ushort address, byte data)
         {
