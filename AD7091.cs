bd257fd7291cad95ed9842efd77381d2513b12ab
Alexey Tseirif IL <alexey.tseirif@satixfy.com>
2025-05-21 17:55:12 +0300

    new AD7091 driver added

---
+++ AD7091.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BringUp_Control
+{
+    internal class AD7091 : IDisposable
+    {
+        private readonly SpiDriver _ft;
+
+
+        public void Dispose()
+        {
+            _ft?.Dispose();
+        }
+    }
+}
