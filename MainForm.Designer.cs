11deeedcbcb8db056d183cfcf07f3ed3c3f5b309
Alexey Tseirif <alexey.tseirif@mda.space>
2025-11-20 13:39:18 +0200

    cp_i option added

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -196,6 +196,8 @@
             this.textLog = new System.Windows.Forms.TextBox();
             this.label20 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label49 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabWelcome.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -268,7 +270,7 @@
             this.tabWelcome.Controls.Add(this.label26);
             this.tabWelcome.Location = new System.Drawing.Point(4, 25);
             this.tabWelcome.Name = "tabWelcome";
-            this.tabWelcome.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabWelcome.Padding = new System.Windows.Forms.Padding(3);
             this.tabWelcome.Size = new System.Drawing.Size(844, 655);
             this.tabWelcome.TabIndex = 8;
             this.tabWelcome.Text = "Welcome";
@@ -295,6 +297,8 @@
             // 
             // tabAD4368
             // 
+            this.tabAD4368.Controls.Add(this.label49);
+            this.tabAD4368.Controls.Add(this.comboBox1);
             this.tabAD4368.Controls.Add(this.label48);
             this.tabAD4368.Controls.Add(this.numericUp_FreqConvert);
             this.tabAD4368.Controls.Add(this.Cmd_Freq_Convert);
@@ -317,7 +321,7 @@
             this.tabAD4368.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabAD4368.Location = new System.Drawing.Point(4, 25);
             this.tabAD4368.Name = "tabAD4368";
-            this.tabAD4368.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabAD4368.Padding = new System.Windows.Forms.Padding(3);
             this.tabAD4368.Size = new System.Drawing.Size(844, 655);
             this.tabAD4368.TabIndex = 0;
             this.tabAD4368.Text = "PLL 4368";
@@ -341,7 +345,7 @@
             0,
             0});
             this.numericUp_FreqConvert.Location = new System.Drawing.Point(606, 402);
-            this.numericUp_FreqConvert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUp_FreqConvert.Margin = new System.Windows.Forms.Padding(2);
             this.numericUp_FreqConvert.Maximum = new decimal(new int[] {
             12801,
             0,
@@ -364,7 +368,7 @@
             // Cmd_Freq_Convert
             // 
             this.Cmd_Freq_Convert.Location = new System.Drawing.Point(606, 432);
-            this.Cmd_Freq_Convert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Cmd_Freq_Convert.Margin = new System.Windows.Forms.Padding(2);
             this.Cmd_Freq_Convert.Name = "Cmd_Freq_Convert";
             this.Cmd_Freq_Convert.Size = new System.Drawing.Size(114, 48);
             this.Cmd_Freq_Convert.TabIndex = 19;
@@ -596,7 +600,7 @@
             this.tabAD9175.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabAD9175.Location = new System.Drawing.Point(4, 25);
             this.tabAD9175.Name = "tabAD9175";
-            this.tabAD9175.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabAD9175.Padding = new System.Windows.Forms.Padding(3);
             this.tabAD9175.Size = new System.Drawing.Size(844, 655);
             this.tabAD9175.TabIndex = 1;
             this.tabAD9175.Text = "DAC 9175";
@@ -1685,7 +1689,7 @@
             // Cmd_FPGA_Import2
             // 
             this.Cmd_FPGA_Import2.Location = new System.Drawing.Point(666, 237);
-            this.Cmd_FPGA_Import2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Cmd_FPGA_Import2.Margin = new System.Windows.Forms.Padding(2);
             this.Cmd_FPGA_Import2.Name = "Cmd_FPGA_Import2";
             this.Cmd_FPGA_Import2.Size = new System.Drawing.Size(145, 41);
             this.Cmd_FPGA_Import2.TabIndex = 21;
@@ -1700,7 +1704,7 @@
             "Uplink DAC0",
             "Uplink DAC1"});
             this.comboBoxDebugger2.Location = new System.Drawing.Point(666, 210);
-            this.comboBoxDebugger2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxDebugger2.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxDebugger2.Name = "comboBoxDebugger2";
             this.comboBoxDebugger2.Size = new System.Drawing.Size(146, 24);
             this.comboBoxDebugger2.TabIndex = 20;
@@ -2131,6 +2135,23 @@
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(205, 97);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 24);
+            this.comboBox1.TabIndex = 22;
+            // 
+            // label49
+            // 
+            this.label49.AutoSize = true;
+            this.label49.Location = new System.Drawing.Point(202, 78);
+            this.label49.Name = "label49";
+            this.label49.Size = new System.Drawing.Size(106, 16);
+            this.label49.TabIndex = 23;
+            this.label49.Text = "CP_I Register:";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -2365,6 +2386,8 @@
         private System.Windows.Forms.NumericUpDown numericUp_FreqConvert;
         private System.Windows.Forms.Button Cmd_Freq_Convert;
         private System.Windows.Forms.Label label48;
+        private System.Windows.Forms.Label label49;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
